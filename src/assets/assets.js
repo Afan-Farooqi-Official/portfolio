c7939cbbae4239a08375843f99bb6c16fcb82914
Afan-Farooqi-Official <affanqaisar000@gmail.com>
2026-02-10 13:27:14 +0500

    fix: correct image file extensions in assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,9 @@
 import logo from './logo.png';
-import AIStudio from './Projects/AIStudio.png';
-import Clothify from './Projects/Clothify.png';
-import ChatOne from './Projects/ChatOne.png';
-import BlogCircle from './Projects/BlogCircle.png';
-import DisneyPlus from './Projects/DisneyPlus.png';
+import AIStudio from './Projects/AIStudio.PNG';
+import Clothify from './Projects/Clothify.PNG';
+import ChatOne from './Projects/ChatOne.PNG';
+import BlogCircle from './Projects/BlogCircle.PNG';
+import DisneyPlus from './Projects/DisneyPlus.PNG';
 
 export const assets = {
     logo,
